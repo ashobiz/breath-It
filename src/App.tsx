bf6025eabbefe91c02a4915c12018121d3c49b48
Ashok <ashobiz@gmail.com>
2022-02-01 17:57:45 +0530

    Added stop

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import styles from './app.module.scss';
+import ColorPicker from './components/colorPicker/colorPicker';
 import SimpleBreathing from './components/simpleBreathing/simpleBreathing';
 
 const App: React.FC = () => {
   return (
     <div className={styles.main}>
       <SimpleBreathing />
+      <ColorPicker />
     </div>
   );
 };
